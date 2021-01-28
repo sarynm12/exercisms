566c363d198c0a4aad3b29bdbf3baa2b81b52c27
Saryn Mooney <smooney15@my.whitworth.edu>
2021-01-27 19:37:09 -0700

    create slices method

--- series/series.rb
+++ series/series.rb
@@ -1,3 +1,14 @@
 class Series
+  attr_reader :string
 
-end 
+  def initialize(string)
+    @string = string
+  end
+
+  def slices(num)
+    raise ArgumentError unless string.length >= num 
+    split = string.split("")
+    split.each_cons(num).map(&:join)
+  end
+
+end
